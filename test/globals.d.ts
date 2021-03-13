2cecdcbbfb70c0eb1b98559a0fc3934a3562b670
Carolina Ale <redrumjedi@gmail.com>
2021-03-13 15:59:08 -0300

    setup initial test

---
+++ test/globals.d.ts
@@ -0,0 +1,7 @@
+//overriding global types, inline imports https://stackoverflow.com/questions/39040108/import-class-in-definition-file-d-ts/51114250#51114250
+
+declare namespace NodeJS {
+  interface Global {
+    testRequest: import("supertest").SuperTest<import("supertest").Test>;
+  }
+}
