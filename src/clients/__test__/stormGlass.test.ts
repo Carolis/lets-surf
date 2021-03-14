925e6c8e78f4e871a74b5e2eb08d440baabd961f
Carolina Ale <redrumjedi@gmail.com>
2021-03-14 19:13:15 -0300

    add unit test and axios setup mock

--- src/clients/__test__/stormGlass.test.ts
+++ src/clients/__test__/stormGlass.test.ts
@@ -1,15 +1,18 @@
 import { StormGlass } from '@src/clients/stormGlass';
 import axios from 'axios';
+import stormGlassWeather3HoursFixture from '@test/fixtures/stormglass_weather_3_hours.json';
+import stormGlassNormalized3HoursFixture from '@test/fixtures/stormglass_normalized_response_3_hours.json';
+
 jest.mock('axios');
 describe('StormGlass client', () => {
   it('should return the normalized forecast from the service ', async () => {
     const lat = -232.2323322;
     const lng = 123.2323322;
 
-    axios.get = jest.fn().mockResolvedValue({});
-    
+    axios.get = jest.fn().mockResolvedValue(stormGlassWeather3HoursFixture);
+
     const stormGlass = new StormGlass(axios);
     const response = await stormGlass.fetchPoints(lat, lng);
-    expect(response).toEqual({});
+    expect(response).toEqual(stormGlassNormalized3HoursFixture);
   });
 });
