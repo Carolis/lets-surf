351ab0c36d0fd0a56230660ce5d2eca4dffe133c
Carolina Ale <redrumjedi@gmail.com>
2021-03-14 18:57:23 -0300

    add stormGlass test

---
+++ src/clients/test/stormGlass.test.ts
@@ -0,0 +1,15 @@
+import { StormGlass } from '@src/clients/stormGlass';
+import axios from 'axios';
+jest.mock('axios');
+describe('StormGlass client', () => {
+  it('should return the normalized forecast from the service ', async () => {
+    const lat = -232.2323322;
+    const lng = 123.2323322;
+
+    axios.get = jest.fn().mockResolvedValue({});
+    
+    const stormGlass = new StormGlass(axios);
+    const response = await stormGlass.fetchPoints(lat, lng);
+    expect(response).toEqual({});
+  });
+});
